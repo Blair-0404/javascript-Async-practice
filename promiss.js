c1f9e6e04a244402bd14a520f46c591328823339
Rachel4858 <rachel458458@gmail.com>
2020-06-30 18:10:58 +0900

    Practice JS Async: 콜백함수, 프라미스, Async/Await, Fetch 연습

--- promiss.js
+++ promiss.js
@@ -30,42 +30,42 @@ function createPost(post) {
   })
 }
 
-// // 프라미스 실행부
-// createPost({title: "Post Three", body: "This is post three"})
-// .then(getPosts)
-// .catch(err => console.log('에러 : ' + err));
+// 프라미스 실행부
+createPost({title: "Post Three", body: "This is post three"})
+.then(getPosts)
+.catch(err => console.log('에러 : ' + err));
 
-// // Promise.all
-// const promise1 = Promise.resolve('Hello World');
-// const promise2 = 10;
-// const promise3 = new Promise((res, rej) => {
-//   setTimeout(res, 2000, 'Goodbye')
-// });
-// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
-//
-// Promise.all([promise1, promise2,promise3, promise4]).then((values) => console.log(values));
+// Promise.all
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((res, rej) => {
+  setTimeout(res, 2000, 'Goodbye')
+});
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+
+Promise.all([promise1, promise2,promise3, promise4]).then((values) => console.log(values));
 
 
 
-// // Async / Await
-// async function init() {
-//   await createPost({title: "Post Three", body: "This is post three"});
-//
-//   getPosts();
-// }
-// init()
+// Async / Await
+async function init() {
+  await createPost({title: "Post Three", body: "This is post three"});
 
-// // Async / Await / Fetch
-// async function fetchUsers() {
-//   const res = await fetch('https://jsonplaceholder.typicode.com/users');
-//   const datas = await res.json();
-//   // console.log(datas);
-//
-//   datas.forEach((data) => {
-//     posts.push({title: data.name, body: data.email});
-//   });
-//   // console.log(posts);
-//   getPosts();
-// }
-//
-// fetchUsers();
+  getPosts();
+}
+init();
+
+// Async / Await / Fetch
+async function fetchUsers() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const datas = await res.json();
+  // console.log(datas);
+
+  datas.forEach((data) => {
+    posts.push({title: data.name, body: data.email});
+  });
+  // console.log(posts);
+  getPosts();
+}
+
+fetchUsers();
