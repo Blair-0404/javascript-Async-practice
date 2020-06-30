09333508c7b347fbe5b3282fdc50f828ade0e981
Rachel4858 <rachel458458@gmail.com>
2020-06-30 19:38:04 +0900

    Practice hard callback

---
+++ hardCourse/callback.js
@@ -0,0 +1,36 @@
+// 콜백 & 콜백지옥
+
+console.log('Start');
+
+function loginUser(email, name, cb) {
+  setTimeout(() => {
+    console.log("Now we have the data");
+    cb({mail: email})
+  }, 2000)
+}
+
+function getUserVideos(email, cb) {
+  setTimeout(() => {
+    cb(['video1', 'video2', 'video3']);
+  }, 1000)
+}
+
+function videoDetail(video, cb) {
+  setTimeout(() => {
+    cb(['title of the video']);
+  }, 1000)
+}
+
+// 콜백 지옥
+const user = loginUser("blair@gmail.com", "blair", (userEmail) => {
+  console.log(userEmail);
+  getUserVideos(userEmail.mail, (videos) => {
+    console.log(videos);
+    videoDetail(videos[0], (title) => {
+      console.log(title);
+    })
+  });
+});
+
+console.log(user);
+console.log('Finish');
